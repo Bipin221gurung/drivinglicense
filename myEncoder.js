8c8e0ca26d2bd65b416c7ef3186b300c9bf5450a
Bipin Gurung <bgurung9022@conestogac.on.ca>
2023-12-15 13:36:17 -0500

    updated the project

---
+++ myEncoder.js
@@ -0,0 +1,8 @@
+// myEncoder.js
+
+const { TextEncoder, TextDecoder } = require('util');
+
+const utf8Encoder = new TextEncoder();
+
+// Export the TextEncoder instance or any other functionality you need
+module.exports = utf8Encoder;
